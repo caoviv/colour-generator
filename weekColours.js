8bd673afab2c73023c05653927491a49e2161ddd
VivianC <viviancao58@gmail.com>
2022-11-07 12:48:36 +1100

    change function from random to cycle

--- weekColours.js
+++ weekColours.js
@@ -2,25 +2,26 @@
 const colours = ["#6667AB", "#939597", "#0f4c81", "#ff6f61"];
 const btn = document.getElementById('btn');
 const colour = document.querySelector('.colour');
+let colourCount = 0;
 
 
 // Event Listeners 
-btn.addEventListener("click", changeColour);
+btn.addEventListener("click", cycleColour);
 
 // Functions
-// changes the background colour 
-function changeColour() {
-    // randomly select colour from colours array 
-    const randomNumber = getRandomNumber();
-    console.log(randomNumber);
-    // set background to the random colour
-    document.body.style.backgroundColor = colours[randomNumber];
-    // set text to colour name, rgb or hex 
-    colour.textContent = colours[randomNumber];
-}
-
-/* randomly select colour from colours array,
-get random number between 0 and the array length (i.e 3)*/ 
-function getRandomNumber() {
-    return Math.floor(Math.random() * colours.length);    
+// cycle throught colours array
+function cycleColour() {
+    if (colourCount < colours.length - 1 ) {
+            // set background to the random colour
+        document.body.style.backgroundColor = colours[colourCount];
+            // set text to colour name, rgb or hex 
+        colour.textContent = colours[colourCount];
+        colourCount += 1;
+    }  else {
+            // set background to the random colour
+        document.body.style.backgroundColor = colours[colourCount];
+            // set text to colour name, rgb or hex 
+        colour.textContent = colours[colourCount];
+        colourCount = 0;
+    }
 }
