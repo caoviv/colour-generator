b8dade91e7c1447a599ff79e0ed9c4c99e968180
VivianC <viviancao58@gmail.com>
2022-11-07 13:00:16 +1100

    add colour name change to cycle function

--- weekColours.js
+++ weekColours.js
@@ -1,7 +1,8 @@
 // Variables 
-const colours = ["#6667AB", "#939597", "#0f4c81", "#ff6f61"];
+const colours = [["#6667AB", "2022 Very Peri"], ["#939597", "2021 Illuminating"], ["#0f4c81", "2020 Classic Blue"], ["#ff6f61", " 2019 Living Coral"]];
 const btn = document.getElementById('btn');
-const colour = document.querySelector('.colour');
+const colourName = document.querySelector('.name');
+const colourHex = document.querySelector('.hex');
 let colourCount = 0;
 
 
@@ -13,15 +14,17 @@ btn.addEventListener("click", cycleColour);
 function cycleColour() {
     if (colourCount < colours.length - 1 ) {
             // set background to the random colour
-        document.body.style.backgroundColor = colours[colourCount];
+        document.body.style.backgroundColor = colours[colourCount][0];
             // set text to colour name, rgb or hex 
-        colour.textContent = colours[colourCount];
+        colourHex.textContent = colours[colourCount][0];
+        colourName.textContent = colours[colourCount][1];
         colourCount += 1;
     }  else {
             // set background to the random colour
-        document.body.style.backgroundColor = colours[colourCount];
+        document.body.style.backgroundColor = colours[colourCount][0];
             // set text to colour name, rgb or hex 
-        colour.textContent = colours[colourCount];
+        colourHex.textContent = colours[colourCount][0];
+        colourName.textContent = colours[colourCount][1];
         colourCount = 0;
     }
 }
