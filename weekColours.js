e92b3a7a144996b6e686fd36eb887e2d7324ec05
VivianC <viviancao58@gmail.com>
2022-11-07 13:15:08 +1100

    add title colour change to cycle function

--- weekColours.js
+++ weekColours.js
@@ -13,17 +13,23 @@ btn.addEventListener("click", cycleColour);
 // cycle throught colours array
 function cycleColour() {
     if (colourCount < colours.length - 1 ) {
-            // set background to the random colour
+        // set title colour to the featuring colour
+        document.querySelector('.title').style.color = colours[colourCount][0];
+        // set background to featuring colour
         document.body.style.backgroundColor = colours[colourCount][0];
-            // set text to colour name, rgb or hex 
+        // set text to colour hex 
         colourHex.textContent = colours[colourCount][0];
+        // set text to colour name 
         colourName.textContent = colours[colourCount][1];
         colourCount += 1;
     }  else {
-            // set background to the random colour
+        // set title colour to the featuring colour
+        document.querySelector('.title').style.color = colours[colourCount][0];
+        // set background to featuring colour
         document.body.style.backgroundColor = colours[colourCount][0];
-            // set text to colour name, rgb or hex 
+        // set text to colour hex 
         colourHex.textContent = colours[colourCount][0];
+        // set text to colour name 
         colourName.textContent = colours[colourCount][1];
         colourCount = 0;
     }
